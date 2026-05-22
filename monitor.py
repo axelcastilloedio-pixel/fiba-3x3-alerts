e9ffa6775484d954a915dc64395042222c1b0935
axelcastilloedio-pixel <axelcastilloedio@gmail.com>
2026-05-22 22:37:46 +0200

    force improved test email

--- monitor.py
+++ monitor.py
@@ -116,8 +116,7 @@ def main():
 
     new_events = [e for e in current if e["name"] not in seen]
 
-    if new_events:
-        send_email(new_events)
+   send_email(current)
 
     save_seen(current_names)
 
